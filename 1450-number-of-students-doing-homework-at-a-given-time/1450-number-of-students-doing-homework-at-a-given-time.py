4d40241e46a3c128dca6ff6a69d76fe620df9d35
KURAKULA HARSHITHA <kurakulaharshitha465@gmail.com>
2026-08-26 16:28:38 +0530

    Time: 0 ms (100%), Space: 12.2 MB (94.25%) - LeetHub

---
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def busyStudent(self, startTime, endTime, queryTime):
+        count=0
+        for i in range(len(endTime)):
+            if endTime[i]>=queryTime and startTime[i]<=queryTime:
+                count=count+1
+        return count
+        
+           
